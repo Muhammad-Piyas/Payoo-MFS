e303ac9d9e2b71102b71ee625e77e5b8d752f423
Muhammad-Piyas <piyas62552@gmail.com>
2024-09-19 09:44:33 +0600

    JS input file Payoo MFS Project

---
+++ Js/login.js
@@ -0,0 +1,12 @@
+// search: form submit reloading the page
+
+// step - 1: set event handler
+document
+  .getElementById("btn-login")
+  .addEventListener("click", function (event) {
+    // step - 2: prevent default behavior (prevent reloading browser)
+    event.preventDefault(); //---> vejal to beginners
+    console.log("addd now");
+
+    // step - 3: get the phone number
+  });
